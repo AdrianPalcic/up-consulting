90ca7ce59f4f550638ca1ce69605af9b21148f0b
Adrian Palcic <adrian.palcic@gmail.com>
2026-03-22 18:16:15 +0100

    some bullshit

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,16 @@ export default function RootLayout({
         gtag('config', 'AW-18004352296');
       `}
     </Script>
+<Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K276V2QF');
+          `}
+        </Script>
+
 
     <link 
       rel="preload" 
@@ -68,6 +78,14 @@ export default function RootLayout({
     />
   </head>
       <body suppressHydrationWarning={true} className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K276V2QF"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Navbar />
         {children}
         <Footer />
